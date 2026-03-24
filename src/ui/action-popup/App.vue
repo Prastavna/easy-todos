87c6ccfd98a9bb08c9bdeb8787998666f93fb763
Sahil Patel <ssahillppatell@gmail.com>
2026-03-24 12:03:45 +0530

    scaffold using viteplus

--- src/ui/action-popup/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <UApp>
-    <main class="w-[800px] h-[600px] flex flex-col">
-      <RouterView />
-    </main>
-  </UApp>
-</template>
--- src/ui/action-popup/views/Settings.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <RouterLink to="/">
-        <UButton variant="ghost" icon="i-heroicons-arrow-left"></UButton>
-    </RouterLink>
-</template>
